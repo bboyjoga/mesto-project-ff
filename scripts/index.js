6117c4f4982b38c03fef274da1fbee80d0f8d5ba
v.zaytsev <bboyjoga@mail.ru>
2024-03-20 13:23:45 +0300

    first

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,18 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+function createCard(card, deleteCard) {
+   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+   const deleteCardButton = cardElement.querySelector('.card__delete-button');
+   cardElement.querySelector('.card__image').src = card.link;
+   cardElement.querySelector('.card__title').textContent = card.name;
+   deleteCardButton.addEventListener('click', () => deleteCard(cardElement));
+   return cardElement;
+}
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function deleteCard(cardElement) {
+   cardElement.remove();
+}
+initialCards.forEach((card) => {
+   placesList.append(createCard(card, deleteCard))
+});
