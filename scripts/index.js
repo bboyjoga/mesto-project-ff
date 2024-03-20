a311fec760774e8c99e2013466d9c373106c2304
v.zaytsev <bboyjoga@mail.ru>
2024-03-20 13:48:38 +0300

    second

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,7 @@ function createCard(card, deleteCard) {
    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
    const deleteCardButton = cardElement.querySelector('.card__delete-button');
    cardElement.querySelector('.card__image').src = card.link;
+   cardElement.querySelector('.card__image').textContent = card.alt;
    cardElement.querySelector('.card__title').textContent = card.name;
    deleteCardButton.addEventListener('click', () => deleteCard(cardElement));
    return cardElement;
